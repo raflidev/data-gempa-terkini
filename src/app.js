d37bd16a615f92c53ab6da08327065658ac2e740
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-04-16 07:41:18 +0700

    port fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000
 const axios = require('axios')
 var convert = require('xml-js');
 var moment = require('moment')
